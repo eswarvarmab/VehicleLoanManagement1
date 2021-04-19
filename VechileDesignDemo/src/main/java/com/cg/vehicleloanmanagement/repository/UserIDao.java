94dcd3d2e6142c24293bcc1abf94a7475017da8f
Eswar-varma <168r1a04c8@gmail.com>
2021-04-19 08:48:47 +0530

    vehicleloan1

---
+++ VechileDesignDemo/src/main/java/com/cg/vehicleloanmanagement/repository/UserIDao.java
@@ -0,0 +1,17 @@
+package com.cg.vehicleloanmanagement.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cg.vehicleloanmanagement.entity.User;
+
+
+
+public interface UserIDao {
+	public List<User> findAll();
+	public Optional<User> findById(String email);
+	public User save(User user);
+	public void deleteById(String email);
+	//public void create_User(User user);
+	
+}
